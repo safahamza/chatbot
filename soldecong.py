54972144029ce32284d16c80286c6b6e0e45c156
SafaHamza <safahamza20@gmail.com>
2018-03-23 10:35:43 +0100

    correction

--- soldecong.py
+++ soldecong.py
@@ -1,58 +1,64 @@
 import urllib
 import json
 import os
-from flask import flask
+from flask import Flask
 from flask import request
 from flask import make_response
-
-app = flask(_name_)
-@app.route('/webhook', method=["POST"])
-def webhook()
+app = Flask(__name__)
+@app.route('/webhook', methods=['POST'])
+def webhook() :
     req = request.get_json(silent=true, force=true)
-	print ("Request:")
-	print(json.dumps(req, indent=4))
-	res = makeWebhookResult(req)
-	res = json.dumps(res, indent=4)
-	print(res)
-	r = make_response(res)
-	r.headers['Content-Type'] = 'application/json'
-	reurn r
-	
-def makeWebhookResult(req)
-    if req.get("result").get("action")!= "soldeConge"
-	   return{}
-	result = req.get("result")
-	parameters = result.get("parameters")  
-	typeConge =  parameters.get("conge")
-	conges={'congées payés':10, 'RTT':5}
-	if typeConge in {"congés", "congé", "conge", "conges"} :
-	   if conges['congées payés']+ conges['RTT'] == 0 :
-	      speech= "Vous n'avez pas de congés"
-	   else:
-	      speech= "Vous avez " conges['congées payés']+ conges['RTT']  " jours de congés"
+    print("Request:")
+    print(json.dumps(req, indent=4))
+    res = makeWebhookResult(req)
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
-	else:
-	     if typeConge == "RTT":
-		    speech = "vous avez"  conges['RTT'] "jours  de RTT"
-			
+def makeWebhookResult(req) :
+    if req.get("result").get("action")!= "soldeConge":
+       return{}
+    result = req.get("result")
+    parameters = result.get("parameters")  
+    typeConge = parameters.get("conge")
+    conges={'congées payés':10, 'RTT':5}
+    #Pas de congés
+    if conges['congées payés']+ conges['RTT'] == 0 :
+       speech="Vous n'avez pas de congés"
+
+
+    #sans spécification du type de congés: Réponse ventilée
+    elif typeConge == "congés" :
+         speech= "Vous avez " + str(conges['congées payés']) + " jours de congés payés et " + str(conges['RTT'])+ "jours deRTT"
+    #spécification du type
+ 
+       #RTT
+    elif typeConge == "RTT":
+          speech = "vous avez"+ str(conges['RTT']) + "jours  de RTT"
+       #congés payés
+    elif typeConge == "congés payés" :
+          speech = "vous avez"+ str(conges['congées payés']) + "jours  de congés payés"
+
     print("Response :")
-	print(speech)
-	return{
-	       "speech": speech,
-		   "displayText": speech
-		   }
-if _name_ == '_main_'
+    print(speech)
+    return{
+           "speech": speech,
+           "displayText": speech
+          }
+if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
     print("Starting app on port %d" % port)
 
     app.run(debug=False, port=port, host='0.0.0.0')
      
-	   
-	
-	
-	
-	
-	
-	
+   
+
+
+
+
+
+
     
